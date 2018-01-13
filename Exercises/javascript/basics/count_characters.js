da3640109b60bb8604d258c041d96d645cf97bdb
Bhavik Patel <Bhavikpatel576@gmail.com>
2018-01-12 18:50:05 -0600

    update information from the launchschool docs

---
+++ Exercises/javascript/basics/count_characters.js
@@ -0,0 +1,12 @@
+
+//Please enter a phrase: walk
+// console output
+// There are 4 characters in "walk".
+//
+// Please enter a phrase: walk, don't run
+// // console output
+// There are 15 characters in "walk, don't run".
+
+var string1 = prompt('Please Enter a phrase');
+console.output(string1.length);
+
